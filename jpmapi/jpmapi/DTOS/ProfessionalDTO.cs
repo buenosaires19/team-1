bd3e41e334e7df9600ed1d6daa8518840162d4d4
mfacundo94 <mfacundo94@gmail.com>
2019-05-04 02:44:04 -0300

    ProfessionalDataRetrieve

---
+++ jpmapi/jpmapi/DTOS/ProfessionalDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jpmapi.DTOS
+{
+    public class ProfessionalDTO
+    {
+        public int profesional_id { get; set; }
+        public string profesional_name { get; set; }
+        public string career { get; set; }
+        public string company { get; set; }
+        public string image { get; set; }
+
+    }
+}
