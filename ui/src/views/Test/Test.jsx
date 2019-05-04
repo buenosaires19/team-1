0dbe528d56b59d82d5a516f2f1790fc39cc78208
aenoris <aenoriss@gmail.com>
2019-05-04 00:33:07 -0300

    wasd

--- ui/src/views/Test/Test.jsx
+++ ui/src/views/Test/Test.jsx
@@ -15,36 +15,58 @@ class Test extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgArray: [img1,img2,img3,img4,img5,],
+      imgArray: [{ img: img1, type: "art" }, { img: img2, type: "economics" }, { img: img3, type: "engineering" }, { img: img4, type: "humanities" }, { img: img5, type: "art" }],
       index: 0,
-      categories: 
-        {
-          arte: 0,
-          economicas: 0,
-          ingenieria: 0,
-          humanidades: 0,
-        },
+      categories:
+      {
+        art: 0,
+        economics: 0,
+        engineering: 0,
+        humanities: 0,
+      },
     };
 
     this.yesFunc = this.yesFunc.bind(this);
     this.noFunc = this.noFunc.bind(this);
   }
 
-  yesFunc(){
-    
-    if(this.state.index < this.state.imgArray.length - 1){
-      this.setState({index: this.state.index + 1}, () => {
+  increase() {
+    switch (this.state.imgArray[this.state.index].type) {
+      case "art":
+        this.setState({ categories: art = this.state.categories.art + 1 })
+        break;
+      case "economics":
+        this.setState({ categories: economics = this.state.categories.economics + 1 })
+        break;
+      case "engineering":
+        this.setState({ categories: engineering = this.state.categories.engineering + 1 })
+        break;
+      case "humanities":
+        this.setState({ categories: humanities = this.state.categories.humanities + 1 })
+        break;
+    }
+  }
+
+  yesFunc() {
+    this.increase()
+
+    if (this.state.index < this.state.imgArray.length - 1) {
+      this.setState({ index: this.state.index + 1 }, () => {
         console.log("arrlgt", this.state.imgArray.length);
         console.log("index", this.state.index);
+        console.log("index", this.state.categories);
       })
     }
   }
 
-  noFunc(){
-    if(this.state.index < this.state.imgArray.length - 1){
-      this.setState({index: this.state.index + 1}, () => {
+  noFunc() {
+    this.increase()
+
+    if (this.state.index < this.state.imgArray.length - 1) {
+      this.setState({ index: this.state.index + 1 }, () => {
         console.log("arrlgt", this.state.imgArray.length);
         console.log("index", this.state.index);
+        console.log("index", this.state.categories);
       })
     }
   }
@@ -61,14 +83,14 @@ class Test extends React.Component {
           <GridContainer justify="center">
             <GridItem xs={4} sm={4} md={4}>
               <Card style={{ background: "white", height: "600px", width: "400px", padding: "50px", margin: "80px" }}>
-                <CardMedia style={{ height: "400px", width: "300px" }} image={this.state.imgArray[this.state.index]} />
+                <CardMedia style={{ height: "400px", width: "300px" }} image={this.state.imgArray[this.state.index].img} />
                 <CardContent>
                   <GridContainer style={{ margin: "30px" }} justify="center">
                     <GridItem xs={6} sm={6} md={6}>
-                      <Button style={{ height:"60px", width:"80px",background:"red",fontSize: "20px"}} onClick = {this.noFunc} variant="contained" color="primary">NO</Button>
+                      <Button style={{ height: "60px", width: "80px", background: "red", fontSize: "20px" }} onClick={this.noFunc} variant="contained" color="primary">NO</Button>
                     </GridItem>
                     <GridItem xs={6} sm={6} md={6}>
-                      <Button style={{ height:"60px", width:"80px", background:"green",fontSize: "20px" }} onClick = {this.yesFunc} variant="contained" color="primary">SI</Button>
+                      <Button style={{ height: "60px", width: "80px", background: "green", fontSize: "20px" }} onClick={this.yesFunc} variant="contained" color="primary">SI</Button>
                     </GridItem>
                   </GridContainer>
                 </CardContent>
